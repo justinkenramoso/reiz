28bc0163d9ec1b3f788da561365852660fae3870
justinkenramoso <justinken.ramoso@gmail.com>
2023-03-09 09:50:49 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/output.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-color4 min-h-screen pt-5 px-5">
+      <div className="container m-auto">
+        <div className="p-0 m-0 bg-color1 text-color4 rounded">
+          <h2 className="p-3 text-3xl font-bold">Country Database</h2>
+        </div>
+        <div className="my-3">
+          <div className="grid grid-cols-2">
+            <div>
+              <button className="font-bold rounded-full inline-block py-2 px-5 mr-3 bg-color2 text-color4">
+                Smaller than Lithuania
+              </button>
+              <button className="font-bold rounded-full inline-block py-2 px-5 bg-color2 text-color4">
+                Oceana Region
+              </button>
+            </div>
+            <div className="ml-auto">
+              <button className="font-bold rounded-full inline-block py-2 px-5 mr-3 bg-color2 text-color4">
+                Ascending
+              </button>
+              <button className="font-bold rounded-full inline-block py-2 px-5 bg-color2 text-color4">
+                Descending
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        color1: "#2C3333",
+        color2: "#2E4F4F",
+        color3: "#0E8388",
+        color4: "#CBE4DE",
+      },
+    },
+  },
+  plugins: [],
+};
